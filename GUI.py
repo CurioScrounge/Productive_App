21ebc611e89af17cc4238594db5b38f3462a9613
Fruitkid <fruinch@gmail.com>
2024-06-23 15:18:05 +0800

    Splash screen complete + Python implementation

--- GUI.py
+++ GUI.py
@@ -1,14 +1,85 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget, QLineEdit, QCheckBox, QTimeEdit, QSpinBox
 
 import sys
+import platform
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 
-def window():
-    app = QApplication(sys.argv) #get system being worked on
-    win=QMainWindow()  #set up window for UI
-    win.setGeometry(100,200,800,600) #Where the window should show up and dimensions
-    win.setWindowTitle("Configure settings")    #Title of window
-    
-    win.show() #To show window
-    sys.exit(app.exec_())  #To ensure when we exit, the application closes properly
+## ==> SPLASH SCREEN
+from ui_Splash import Ui_SplashScreen
 
-window() #call function to show window
+#from ui_UnproductiveSelections import Ui_MainWindow
+
+## ==> GLOBALS
+counter = 0
+
+# # YOUR APPLICATION
+# class UnproductiveSelections(QMainWindow):
+#     def __init__(self):
+#         QMainWindow.__init__(self)
+#         self.ui = Ui_MainWindow()
+#         self.ui.setupUi(self)
+
+
+# SPLASH SCREEN
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_SplashScreen()
+        self.ui.setupUi(self)
+
+        ## UI ==> INTERFACE CODES
+        ########################################################################
+
+        ## REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+
+        ## DROP SHADOW EFFECT
+
+        ## QTIMER ==> START
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        # TIMER IN MILLISECONDS
+        self.timer.start(35)
+
+
+        ## SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+        ## ==> END ##
+
+    ## ==> APP FUNCTIONS
+    ########################################################################
+    def progress(self):
+
+        global counter
+
+        # SET VALUE TO PROGRESS BAR
+        self.ui.progressBar.setValue(counter)
+
+        # CLOSE SPLASH SCREE AND OPEN APP
+        if counter > 100:
+            
+            # STOP TIMER
+            self.timer.stop()
+            
+            #Show main window
+            # self.main = MainWindow()
+            # self.main.show()
+            
+            # CLOSE SPLASH SCREEN
+            self.close()
+
+        # INCREASE COUNTER
+        counter += 1
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SplashScreen()
+    sys.exit(app.exec_())
---
+++ ui_Splash.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'SplashuknrEm.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PyQt5.QtWidgets import *
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+
+
+class Ui_SplashScreen(object):
+    def setupUi(self, SplashScreen):
+        if not SplashScreen.objectName():
+            SplashScreen.setObjectName(u"SplashScreen")
+        SplashScreen.resize(974, 677)
+        self.centralwidget = QWidget(SplashScreen)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        font = QFont()
+        font.setPointSize(30)
+        self.frame.setFont(font)
+        self.frame.setStyleSheet(u"QFrame{\n"
+" \n"
+"	background-color: rgb(55, 58, 64);\n"
+"}\n"
+"")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.label_title = QLabel(self.frame)
+        self.label_title.setObjectName(u"label_title")
+        self.label_title.setGeometry(QRect(0, 190, 951, 91))
+        font1 = QFont()
+        font1.setFamily(u"Segoe UI")
+        font1.setPointSize(36)
+        font1.setBold(False)
+        font1.setWeight(50)
+        self.label_title.setFont(font1)
+        self.label_title.setStyleSheet(u"QLabel{\n"
+"color: rgb(255, 116, 70);\n"
+"}\n"
+"")
+        self.label_title.setTextFormat(Qt.AutoText)
+        self.label_title.setAlignment(Qt.AlignCenter)
+        self.progressBar = QProgressBar(self.frame)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(110, 520, 731, 23))
+        self.progressBar.setStyleSheet(u"QProgressBar{\n"
+"text-align:center;\n"
+"	background-color: rgb(238, 238, 238);\n"
+"color:rgb(55, 58, 64);\n"
+"border-style:none;\n"
+"border-radius:10px;\n"
+"}\n"
+"\n"
+"QProgressBar::chunk{\n"
+"border-radius:10px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0.506, x2:1, y2:0.489, stop:0 rgba(220, 95, 0, 255), stop:1 rgba(204, 68, 0, 255));\n"
+"}")
+        self.progressBar.setValue(24)
+        self.label_phrase = QLabel(self.frame)
+        self.label_phrase.setObjectName(u"label_phrase")
+        self.label_phrase.setGeometry(QRect(10, 270, 951, 51))
+        font2 = QFont()
+        font2.setFamily(u"Segoe UI")
+        font2.setPointSize(14)
+        font2.setBold(False)
+        font2.setWeight(50)
+        self.label_phrase.setFont(font2)
+        self.label_phrase.setStyleSheet(u"QLabel{\n"
+"	color: rgb(255, 255, 255);\n"
+"}")
+        self.label_phrase.setTextFormat(Qt.AutoText)
+        self.label_phrase.setAlignment(Qt.AlignCenter)
+        self.label_status = QLabel(self.frame)
+        self.label_status.setObjectName(u"label_status")
+        self.label_status.setGeometry(QRect(0, 550, 951, 51))
+        font3 = QFont()
+        font3.setFamily(u"Segoe UI")
+        font3.setPointSize(12)
+        font3.setBold(False)
+        font3.setWeight(50)
+        self.label_status.setFont(font3)
+        self.label_status.setStyleSheet(u"QLabel{\n"
+"	color: rgb(255, 255, 255);\n"
+"}")
+        self.label_status.setTextFormat(Qt.AutoText)
+        self.label_status.setAlignment(Qt.AlignCenter)
+        self.label_credits = QLabel(self.frame)
+        self.label_credits.setObjectName(u"label_credits")
+        self.label_credits.setGeometry(QRect(0, 610, 951, 51))
+        font4 = QFont()
+        font4.setFamily(u"Segoe UI")
+        font4.setPointSize(10)
+        font4.setBold(False)
+        font4.setWeight(50)
+        self.label_credits.setFont(font4)
+        self.label_credits.setStyleSheet(u"QLabel{\n"
+"	color: rgb(255, 255, 255);\n"
+"}")
+        self.label_credits.setTextFormat(Qt.AutoText)
+        self.label_credits.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.label_phrase.raise_()
+        self.label_title.raise_()
+        self.progressBar.raise_()
+        self.label_status.raise_()
+        self.label_credits.raise_()
+
+        self.verticalLayout.addWidget(self.frame)
+
+        SplashScreen.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(SplashScreen)
+
+        QMetaObject.connectSlotsByName(SplashScreen)
+    # setupUi
+
+    def retranslateUi(self, SplashScreen):
+        SplashScreen.setWindowTitle(QCoreApplication.translate("SplashScreen", u"MainWindow", None))
+        self.label_title.setText(QCoreApplication.translate("SplashScreen", u"<html><head/><body><p><span style=\" font-weight:600; color:#dc5f00;\">UNLEASH</span><span style=\" color:#dc5f00;\"> YOUR PRODUCTIVE POTENTIAL</span></p></body></html>", None))
+        self.label_phrase.setText(QCoreApplication.translate("SplashScreen", u"<html><head/><body><p><span style=\" color:#eeeeee;\">Greatly customisable to your benefit, maximise your productivity today!</span></p></body></html>", None))
+        self.label_status.setText(QCoreApplication.translate("SplashScreen", u"loading...", None))
+        self.label_credits.setText(QCoreApplication.translate("SplashScreen", u"<strong>Created:</strong> Summer Guo", None))
+    # retranslateUi
+
